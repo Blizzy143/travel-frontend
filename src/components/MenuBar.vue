6e50c96ffad1e96d6f8dc8be0558d48dec991138
KRISHNA VAMSI NARLA <knarla@uco.edu>
2023-05-30 17:42:20 -0500

    menu changes

--- src/components/MenuBar.vue
+++ src/components/MenuBar.vue
@@ -7,7 +7,7 @@ import UserServices from "../services/UserServices";
 const router = useRouter();
 
 const user = ref(null);
-const title = ref("Travel frontend");
+const title = ref("Bon Voyage");
 const logoURL = ref("");
 
 onMounted(() => {
